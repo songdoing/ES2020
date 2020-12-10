fd57b9b04adb5760300a7307c893bec0cd381b40
wonjinsong <msmk22239@gmail.com>
2020-12-09 21:02:17 -0500

    2. numberBaseball

--- numberBaseball/index.js
+++ numberBaseball/index.js
@@ -2,13 +2,21 @@ const input = document.querySelector("#input");
 const check = document.querySelector("#check");
 const logs = document.querySelector("#logs");
 const error = document.querySelector("#error");
-
 let answer;
+let answerArr = [];
+
+for (let i = 0; i <= 3; i++) {
+  answerArr[i] = Math.floor(Math.random() * 10);
+}
+//from array to String
+answer = String(answerArr.join(""));
+console.log(answer);
+
 let count = 0;
 const randomAnswer = () => {
   //min(in) ~ max(in)
   //Math.floor(Math.random() * (max - min + 1)) + min;
-  answer = Math.floor(Math.random() * 9000) + 1000;
+  //answer = Math.floor(Math.random() * 9000) + 1000;
   console.log(answer);
 };
 check.addEventListener("click", () => {
@@ -19,7 +27,7 @@ check.addEventListener("click", () => {
         "Sorry. You already tried 10 times. The answer is " +
         answer +
         " . Try another one.";
-      randomAnswer();
+      //randomAnswer();
     } else {
       //baseball logs
     }
@@ -28,4 +36,4 @@ check.addEventListener("click", () => {
   }
   count++;
 });
-randomAnswer();
+//randomAnswer();
