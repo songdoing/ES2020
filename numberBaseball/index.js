c258b85ebec6722d4a34ef46244165accef7254e
wonjinsong <msmk22239@gmail.com>
2020-12-11 15:16:34 -0500

    bug fixed

--- numberBaseball/index.js
+++ numberBaseball/index.js
@@ -19,19 +19,31 @@ for (let i = 0; i <= 3; i++) {
 //from array to String
 answer = String(answerArr.join(""));
 console.log(answer);
+input.focus();
+
+const againGame = () => {
+  location.reload();
+};
 
 check.addEventListener("click", () => {
   const tryAnswer = input.value;
   if (tryAnswer && tryAnswer.length === 4) {
-    if (count > 10) {
+    if (count >= 9) {
       error.textContent = `Game over! The answer was ${answer}. Try another one.`;
       const btn = document.createElement("button");
+      btn.setAttribute("id", "againBtn");
       btn.appendChild(document.createTextNode("Again"));
       error.appendChild(btn);
+      btn.addEventListener("click", againGame);
     } else {
       //baseball logs
       if (tryAnswer === answer) {
-        logs.appendChild(document.createTextNode("HR!!!"));
+        logs.appendChild(document.createTextNode("HOME RUN!!! One more?"));
+        const btn = document.createElement("button");
+        btn.setAttribute("id", "againBtn");
+        btn.appendChild(document.createTextNode("Again"));
+        error.appendChild(btn);
+        btn.addEventListener("click", againGame);
       } else {
         console.log("different");
         let strike = 0;
@@ -47,15 +59,22 @@ check.addEventListener("click", () => {
             }
           }
         }
+        input.value = "";
+        input.focus();
+
         const message = document.createTextNode(
-          `${count + 1}. ${tryAnswer} : ${strike} strike, ${ball} ball`
+          `${count + 1}.${tryAnswer} : ${strike} strike, ${ball} ball`
         );
         logs.appendChild(message);
         logs.appendChild(document.createElement("br"));
+        error.textContent = "";
       }
     }
   } else {
     error.textContent = "You should input the 4 digits.";
+    logs.appendChild(document.createTextNode("You missed 1 chance."));
+    logs.appendChild(document.createElement("br"));
+    input.focus();
   }
   count++;
 });
