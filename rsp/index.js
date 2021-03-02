81333b65c58d2cc66cba881b3404fc5013fd71bc
wonjinsong <msmk22239@gmail.com>
2021-03-01 22:47:36 -0500

    setInterval

--- rsp/index.js
+++ rsp/index.js
@@ -1,2 +1,20 @@
 const computerTag = document.querySelector("#computer");
 computerTag.style.background = `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) 0 0`;
+
+let coord = "0";
+const rspCoord = {
+  rock: "0",
+  scissors: "-142px",
+  paper: "-288px",
+};
+
+setInterval(() => {
+  if (coord === rspCoord.rock) {
+    coord = rspCoord.scissors;
+  } else if (coord === rspCoord.scissors) {
+    coord = rspCoord.paper;
+  } else if (coord === rspCoord.paper) {
+    coord = rspCoord.rock;
+  }
+  computerTag.style.background = `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${coord} 0`;
+}, 100);
