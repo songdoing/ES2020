81b1132af3bde51e1ce7e1ec5a64495a2a6f9a4c
wonjinsong <msmk22239@gmail.com>
2021-03-02 23:14:18 -0500

    fisher yates shuffle

--- mineSweeper/index.js
+++ mineSweeper/index.js
@@ -5,18 +5,31 @@ document.querySelector("#exec").addEventListener("click", () => {
 
   console.log(hor, ver, mine);
 
-  let dataset = [];
+  //mine 무작위로 심기, 0~99까지에서 20개 뽑기
+  const candidate = Array(hor * ver)
+    .fill()
+    .map((v, i) => i + 1);
+  //v : 요소  i : 인덱스
+  console.log(candidate); //배열로 쭉 나열됨
+
+  const shuffle = [];
+  while (candidate.length > 80) {
+    const random = Math.floor(Math.random() * candidate.length);
+    const spliceArray = candidate.splice(random, 1); //랜덤된 숫자가 하나하나 배열형태로 리턴
+    console.log(spliceArray);
+    const value = spliceArray[0]; // 배열형태에서 숫자만 뽑기하려면
+    shuffle.push(value);
+  }
+  console.log(shuffle);
+
+  //테이블 만들기
   const tbody = document.querySelector("#table tbody");
   for (let i = 0; i < ver; i += 1) {
-    let arr = [];
     let tr = document.createElement("tr"); //세로를 먼저
     for (let j = 0; j < hor; j += 1) {
-      arr.push(1);
       let td = document.createElement("td");
       tr.appendChild(td);
-      td.textContent = 1;
     }
     tbody.appendChild(tr);
   }
-  console.log(dataset);
 });
