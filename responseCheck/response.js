e774acbd0024e859caa6d73d05936f0e405c9feb
wonjinsong <msmk22239@gmail.com>
2021-03-31 23:49:00 -0400

    response speed

--- responseCheck/response.js
+++ responseCheck/response.js
@@ -1,15 +1,25 @@
 let screen = document.querySelector("#screen");
 let state = {};
+
 screen.addEventListener("click", () => {
+  let startTime;
+  let endTime;
+
   if (screen.classList.contains("waiting")) {
     screen.classList.remove("waiting");
     screen.classList.add("ready");
     screen.textContent = "Click when it changes green.";
+    setTimeout(() => {
+      startTime = new Date();
+      screen.click();
+    }, Math.floor(Math.random() * 1000) + 2000); //2000~3000 랜덤수
   } else if (screen.classList.contains("ready")) {
     screen.classList.remove("ready");
     screen.classList.add("now");
     screen.textContent = "Click Now!";
   } else if (screen.classList.contains("now")) {
+    endTime = new Date();
+    console.log("response speed ", endTime - startTime, " ms");
     screen.classList.remove("now");
     screen.classList.add("waiting");
     screen.textContent = "Click to start.";
