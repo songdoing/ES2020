a20dd669119da48951b8f5f6bd4f319e0c7ab31f
wonjinsong <msmk22239@gmail.com>
2021-04-05 23:48:07 -0400

    computer turn 1

--- ticTacToe/index.js
+++ ticTacToe/index.js
@@ -4,6 +4,59 @@ let rows = [];
 let columns = [];
 let turn = "X";
 let result = document.createElement("div");
+//let win;
+
+let resultCheck = (whichRow, whichColumn) => {
+  //세 칸 찼는지 확인
+  let win = false;
+  //가로줄
+  if (
+    columns[whichRow][0].textContent === turn &&
+    columns[whichRow][1].textContent === turn &&
+    columns[whichRow][2].textContent === turn
+  ) {
+    win = true;
+  }
+  //세로줄
+  if (
+    columns[0][whichColumn].textContent === turn &&
+    columns[1][whichColumn].textContent === turn &&
+    columns[2][whichColumn].textContent === turn
+  ) {
+    win = true;
+  }
+  //대각선
+  if (
+    columns[0][0].textContent === turn &&
+    columns[1][1].textContent === turn &&
+    columns[2][2].textContent === turn
+  ) {
+    win = true;
+  }
+  if (
+    columns[0][2].textContent === turn &&
+    columns[1][1].textContent === turn &&
+    columns[2][0].textContent === turn
+  ) {
+    win = true;
+  }
+  return win; //columnCheck함수의 스코프체인에 없기 때문에
+  //resultCheck함수에 return을 해주면, 전역변수처럼 가져다 쓸수.
+};
+
+let initialize = () => {
+  // 초기화
+  setTimeout(() => {
+    turn = "X";
+    result.textContent = "";
+    columns.forEach(function (row) {
+      row.forEach(function (column) {
+        column.textContent = "";
+        column.style.backgroundColor = "white";
+      });
+    });
+  }, 2000);
+};
 
 let columnCheck = (e) => {
   console.log(e.target); //클릭한 애(태그)
@@ -13,20 +66,6 @@ let columnCheck = (e) => {
   let whichRow = rows.indexOf(e.target.parentNode);
   let whichColumn = columns[whichRow].indexOf(e.target);
   console.log("whichRow:", whichRow, "whichColumn:", whichColumn);
-
-  let initialize = () => {
-    // 초기화
-    setTimeout(() => {
-      turn = "X";
-      result.textContent = "";
-      columns.forEach(function (row) {
-        row.forEach(function (column) {
-          column.textContent = "";
-          column.style.backgroundColor = "white";
-        });
-      });
-    }, 2000);
-  };
 
   let colorize = (turn) => {
     console.log(turn);
@@ -47,40 +86,7 @@ let columnCheck = (e) => {
     columns[whichRow][whichColumn].textContent = turn;
     console.log(turn);
     colorize(turn);
-
-    //세 칸 찼는지 확인
-    let win = false;
-    //가로줄
-    if (
-      columns[whichRow][0].textContent === turn &&
-      columns[whichRow][1].textContent === turn &&
-      columns[whichRow][2].textContent === turn
-    ) {
-      win = true;
-    }
-    //세로줄
-    if (
-      columns[0][whichColumn].textContent === turn &&
-      columns[1][whichColumn].textContent === turn &&
-      columns[2][whichColumn].textContent === turn
-    ) {
-      win = true;
-    }
-    //대각선
-    if (
-      columns[0][0].textContent === turn &&
-      columns[1][1].textContent === turn &&
-      columns[2][2].textContent === turn
-    ) {
-      win = true;
-    }
-    if (
-      columns[0][2].textContent === turn &&
-      columns[1][1].textContent === turn &&
-      columns[2][0].textContent === turn
-    ) {
-      win = true;
-    }
+    let win = resultCheck(); //return win해줘서 스코프체인밖이라도 받을수 있다
 
     if (win) {
       //승리
@@ -118,7 +124,28 @@ let columnCheck = (e) => {
         setTimeout(() => {
           console.log("computer turn");
           //빈칸중 하나를 고른다
+          let emptyColumn = [];
+          columns.forEach((row) => {
+            row.forEach((column) => {
+              emptyColumn.push(column);
+            });
+          });
+          //filter함수는 return이 true인 경우만 filter처리한다.
+          emptyColumn = emptyColumn.filter((column) => {
+            return column.textContent;
+          });
+          console.log(emptyColumn);
+          let selectColumn =
+            emptyColumn[Math.floor(Math.random() * emptyColumn.length)];
+          selectColumn.textContent = turn;
 
+          //컴터 승리했는지 체크한다.
+          let win = resultCheck();
+          if (win) {
+            //승리
+            result.textContent = turn + "'s WIN";
+            initialize();
+          }
           //나에게 턴을 넘긴다
           turn = "x";
         }, 1000);
